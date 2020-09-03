4dba57dcb994aead8dd1aafa445ec54e36b818da
cecily1009 <cecily1009@gmail.com>
2020-09-03 00:00:47 -0400

    Update server.js
    setup for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
+
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(process.env.DATABASE_URI, {
@@ -22,7 +24,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
-app.use(express.static('client/build'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}...`));
